4333eba5a4eed87ee2ca27a5557fe78df382c549
immobarok <immobarokali2001@gmail.com>
2026-07-17 21:19:40 +0600

    feat: implement ProductService to handle product creation with nested specifications and inclusions

--- app/services/product_service.py
+++ app/services/product_service.py
@@ -1,24 +1,35 @@
 from app.repositories.product_repository import ProductRepository
 from app.schemas.product import ProductCreate
-from app.models import Product
+from app.models.product import Product, ProductSpecification, ProductInclude
 from sqlalchemy.orm import Session
+
 class ProductService:
     @staticmethod
-    def create_product(db:Session, product_in:ProductCreate)->Product:
+    def create_product(db: Session, product_in: ProductCreate) -> Product:
+        # Build the main object
         db_product = Product(
             category_id=product_in.category_id,
             name=product_in.name,
-            description=product_in.description,
+            slug=product_in.slug,
+            sku=product_in.sku,
+            badge=product_in.badge,
+            short_description=product_in.short_description,
+            long_description=product_in.long_description,
             price=product_in.price,
-            image_url=product_in.image_url,
-            weight_kg=product_in.weight_kg,
-            is_active=product_in.is_active
+            stock_quantity=product_in.stock_quantity,
+            is_ready_stock=product_in.is_ready_stock,
+            range_km=product_in.range_km,
+            flight_time_min=product_in.flight_time_min,
+            tags=product_in.tags,
+            main_image_url=product_in.main_image_url
         )
-
+        
+        # Build nested lists
         for spec in product_in.specifications:
-            db_product.specifications.append(**spec.model_dump())
-        for item in product_in.items_included:
-            db_product.items_included.append(item_name=item.name)
-            return ProductRepository.create(db, db_product)
-        
-        
+            db_product.specifications.append(ProductSpecification(**spec.model_dump()))
+            
+        for item in product_in.includes:
+            db_product.includes.append(ProductInclude(item_name=item.name))
+            
+        # Hand it to the Repository to save!
+        return ProductRepository.create(db, db_product)
